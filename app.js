8d03f4da47f712774fc197f03242df165a7c7b0b
mia <mia.jurdana@gmail.com>
2021-12-08 00:53:37 +0100

    app use cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 require('dotenv').config();
 
@@ -14,6 +15,7 @@ const userRouter = require('./routes/user')
 
 
 const app = express();
+app.use(cors());
 const { Sequelize } = require('sequelize');
 const sequelize = require('./config/database');
 
